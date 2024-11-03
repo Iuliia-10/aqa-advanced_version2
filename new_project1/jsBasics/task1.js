7eff1ddafbb86e9481569b69c1144e6646600c9d
Iuliia-10 <golynska.iuliia@gmail.com>
2024-11-03 12:31:26 +0200

    added task1.js

---
+++ new_project1/jsBasics/task1.js
@@ -0,0 +1,12 @@
+let errorMessage1 = 'User not found';
+let userStatus = 1;
+let telegramMain = true;
+let userPhoto = null;
+let gender = undefined;
+
+
+console.log(errorMessage1)
+console.log(userStatus)
+console.log(telegramMain)
+console.log(userPhoto)
+console.log(gender)
