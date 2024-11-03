5807adca5269ae256ff94d87b97fce387cedc131
Iuliia-10 <golynska.iuliia@gmail.com>
2024-11-03 13:26:18 +0200

    added tast2.js

---
+++ new_project1/jsBasics/task2.js
@@ -0,0 +1,9 @@
+let studentName = 'Anna';
+let teacherName = 'Victor';
+
+let notification1 = 'Have a nice day, ' + studentName + ' and ' + teacherName;
+
+let notification2 = `Have a nice day, ${studentName} and ${teacherName}`
+
+console.log(notification1)
+console.log(notification2)
